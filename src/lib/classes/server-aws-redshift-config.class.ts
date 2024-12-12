6220fe95f9a15615a850d467f442c800df6ae323
icedlee337 <lee.e.norris@gmail.com>
2024-12-12 02:24:29 -0600

    fix: config

--- src/lib/classes/server-aws-redshift-config.class.ts
+++ src/lib/classes/server-aws-redshift-config.class.ts
@@ -1,7 +1,6 @@
 export class ServerAwsRedshiftDataConfig {
-    AWS_ACCESS_KEY_ID: string;
-    AWS_KINESIS_NAME: string;
+    AWS_ACCESS_KEY_ID?: string;
     AWS_REGION: string;
-    AWS_SECRET_ACCESS_KEY: string;
+    AWS_SECRET_ACCESS_KEY?: string;
     NODE_ENV: string;
 }
